9807dfaa32d1b92e381d5b4bdeb7a7d176cb1d45
stephenprin <stephenprince427@gmail.com>
2025-01-16 16:06:51 -0800

    add security- extending the identityuser

---
+++ Models/AddUser.cs
@@ -0,0 +1,6 @@
+﻿namespace Stock_Market_WebAPI.Models
+{
+    public class AddUser
+    {
+    }
+}
--- obj/Debug/net8.0/Stock_Market_WebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Stock_Market_WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stock_Market_WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed7439d0734378edda20bf8b4df542a7d9a5667f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db14b74c14af77c5072dde7c730d9848d902ea1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stock_Market_WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stock_Market_WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
