d167af45698dfa9781a85a5c410e7bd115e3b5f3
stephenprin <stephenprince427@gmail.com>
2025-01-23 18:13:58 -0800

    implement portfolio

--- obj/Debug/net8.0/Stock_Market_WebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Stock_Market_WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stock_Market_WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9262e09c52a92d3d4f32d1f383b527f8bd6d8f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e286a61b88ac00db22b7aa0187b36613d5343dd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stock_Market_WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stock_Market_WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
