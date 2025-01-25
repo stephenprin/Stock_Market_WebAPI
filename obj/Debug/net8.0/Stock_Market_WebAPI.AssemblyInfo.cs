4400f6d4ff4377e5391af63e6696aa6e2f0431a2
stephenprin <stephenprince427@gmail.com>
2025-01-25 00:01:40 -0800

    fix

--- obj/Debug/net8.0/Stock_Market_WebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Stock_Market_WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stock_Market_WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7184b6c313907cda7a86a6c27acb62e14a58baec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1f3f90907307004ffa14f3445173cb8077b4cd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stock_Market_WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stock_Market_WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
