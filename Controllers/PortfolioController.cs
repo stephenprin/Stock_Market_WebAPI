18334911f5c7c22937a4667272ee1c29462cbc55
stephenprin <stephenprince427@gmail.com>
2025-01-25 00:26:17 -0800

    fix

---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Stock_Market_WebAPI.Interfaces;
+using Stock_Market_WebAPI.Models;
+
+namespace Stock_Market_WebAPI.Controllers
+{
+    [Route("api/portfolio")]
+    [ApiController]
+    public class PortfolioController
+    {
+        public PortfolioController(UserManager<AddUser> userManager, IStockRepository stockRepository)
+        {
+            
+        }
+    }
+}
